89e8f7a8ae3d06a0ce0b05376704362237dfaf58
yatinagg <yatinagg12@gmail.com>
2022-01-11 14:12:19 +0530

    Added Language, Language Color, Stars and Forks

--- app/src/main/java/com/yatinagg/trendingrepositories/adapter/RepositoryAdapter.kt
+++ app/src/main/java/com/yatinagg/trendingrepositories/adapter/RepositoryAdapter.kt
@@ -2,9 +2,11 @@ package com.yatinagg.trendingrepositories.adapter
 
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import com.yatinagg.trendingrepositories.R
 import com.yatinagg.trendingrepositories.databinding.ListviewRepositoriesBinding
 import com.yatinagg.trendingrepositories.model.TrendingRepositories
 import com.yatinagg.trendingrepositories.model.TrendingRepositoriesItem
@@ -29,8 +31,19 @@ class RepositoryAdapter : RecyclerView.Adapter<MainViewHolder>() {
         val repository = repositories[holder.adapterPosition]
         holder.binding.repositoriesText.text = repository.username
         holder.binding.repositoriesText.setBackgroundColor(Color.WHITE)
+        holder.binding.repositoriesName.text = repository.repositoryName
+        holder.binding.repositoriesName.setBackgroundColor(Color.WHITE)
         holder.binding.repositoriesDescription.text = repository.description
-        holder.binding.repositoriesDescription.setBackgroundColor(Color.WHITE)
+        holder.binding.tvRepositoriesLanguage.text = repository.language
+        holder.binding.ivRepositoriesLanguage.setBackgroundColor(Color.parseColor(repository.languageColor))
+        holder.binding.tvRepositoriesStars.text = repository.totalStars.toString()
+        holder.binding.tvRepositoriesForks.text = repository.forks.toString()
+        holder.binding.cardView.setOnClickListener {
+            if(holder.binding.expandableLayout.visibility == View.GONE)
+                holder.binding.expandableLayout.visibility = View.VISIBLE
+            else
+                holder.binding.expandableLayout.visibility = View.GONE
+        }
         Picasso.get().load(repository.builtBy[0].avatar).into(holder.binding.ivAvatar)
 
         println("MainActivity 1 ${repository.url} now $repository")
